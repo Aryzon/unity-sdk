9706d9e615068b13e250bf5b10d01c2d97b327ba
maartenslaa <maartenslaa@me.com>
2021-05-19 16:36:07 +0200

    Fixes

---
+++ Runtime/Scripts/InactivateAtStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InactivateAtStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

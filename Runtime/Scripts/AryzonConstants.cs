ea00a3146e160afe42538d468834377806e1448b
maartenslaa <maartenslaa@users.noreply.github.com>
2023-02-15 21:25:43 +0100

    Update AryzonConstants.cs

--- Runtime/Scripts/AryzonConstants.cs
+++ Runtime/Scripts/AryzonConstants.cs
@@ -7,7 +7,7 @@ namespace Aryzon
     public static class Constants
     {
 #if UNITY_ANDROID
-        public const string CardboardApi = "cardboard_api";
+        public const string CardboardApi = "GfxPluginCardboard";
 #elif UNITY_IOS
         public const string CardboardApi = "__Internal";
 #else
